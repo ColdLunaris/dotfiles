211ed2cbcbad32a8e90c7d2a8181e7f6e0caffcc
Aiden Cronen <azurefrost@protonmail.com>
2021-01-09 18:50:16 +0100

    Add i3status config

---
+++ .config/i3status/click_time.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cal -y		# display year calendar
+read -n 1 -r -s	# wait for touch key to exit
---
+++ .config/i3status/cpu.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+import psutil
+print(psutil.cpu_percent(interval=1), end='')
