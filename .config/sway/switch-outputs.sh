d1f098e93812d27ed43000f08299e916dc98704d
Lunaris <azurefrost@protonmail.com>
2022-03-20 22:52:35 +0100

    Fix symlinks

---
+++ .config/sway/switch-outputs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get current active sink
+CURRENT_SINK=$(pactl list sinks | awk '/RUNNING/ { getline; print $0}')
+USB_DEVICE=$(lsusb | awk '/HyperX/')
+
+# Set external speakers as output if USB headet is currently master
+if [[ $CURRENT_SINK == *"Kingston_HyperX_Virtual_Surround_Sound"* ]]; then
+    pactl set-card-profile alsa_card.pci-0000_0c_00.4 output:analog-stereo
+    pactl set-card-profile alsa_card.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00 off
+    pactl set-default-sink alsa_output.pci-0000_0c_00.4.analog-stereo
+# Set USB headset as output if external speakers are currently master
+else
+    if [[ $USB_DEVICE == *"HyperX"* ]]; then
+        pactl set-card-profile alsa_card.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00 output:analog-stereo
+	pactl set-default-sink alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00.analog-stereo
+    fi
+fi
