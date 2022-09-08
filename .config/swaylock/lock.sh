707278306d217ee9847faba1bb8e47ce6db1bb1f
Lunaris <azurefrost@protonmail.com>
2022-09-08 21:19:07 +0200

    Updated files

--- .config/swaylock/lock.sh
+++ .config/swaylock/lock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+MOUSE=$(ratbagctl | sed 's/ *\:.*//')
+
 swaymsg "output * dpms off"
 openrgb -d 0 -m static -c 000000
 ckb-next -p main -m off
-ratbagctl warbling-mara led 0 set color 000000
+ratbagctl $MOUSE led 0 set color 000000
--- .config/swaylock/unlock.sh
+++ .config/swaylock/unlock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+MOUSE=$(ratbagctl | sed 's/ *\:.*//')
+
 ckb-next -p main -m blue
-ratbagctl warbling-mara led 0 set color 03FDFC
+ratbagctl $MOUSE led 0 set color 03FDFC
 openrgb -d 0 -m static -c 03FDFC
 swaymsg "output * dpms on"
