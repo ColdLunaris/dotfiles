de7ffcd71dd5d3c5b1cd2957f2a6175618f41662
Lunaris <azurefrost@protonmail.com>
2023-08-04 16:05:09 +0200

    Fixed Waybar-config and OpenRGB

--- .config/swaylock/lock.sh
+++ .config/swaylock/lock.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 MOUSE=$(ratbagctl | sed 's/ *\:.*//')
 
-swaymsg "output * dpms off"
-flatpak run org.openrgb.OpenRGB -d 0 -m static -c 000000
-ckb-next -p main -m off
-ratbagctl $MOUSE led 0 set color 000000
+swaymsg "output * dpms off" & 
+flatpak run org.openrgb.OpenRGB -d "ASUS ROG STRIX X570-F GAMING" -z 0 -m static -c 000000 &
+ckb-next -p main -m off &
+ratbagctl $MOUSE led 0 set color 000000 &
+
+wait
--- .config/swaylock/unlock.sh
+++ .config/swaylock/unlock.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 MOUSE=$(ratbagctl | sed 's/ *\:.*//')
 
-ckb-next -p main -m blue
-ratbagctl $MOUSE led 0 set color 03FDFC
-flatpak run org.openrgb.OpenRGB -d 0 -m static -c 03FDFC
-swaymsg "output * dpms on"
+ckb-next -p main -m blue &
+ratbagctl $MOUSE led 0 set color 03FDFC &
+flatpak run org.openrgb.OpenRGB -d "ASUS ROG STRIX X570-F GAMING" -z 0 -m static -c 03FDFC &
+swaymsg "output * dpms on" &
+
+wait
