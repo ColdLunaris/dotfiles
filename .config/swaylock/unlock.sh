c49fed55f6d2abe6a0353eff962e83c120809019
Lunaris <azurefrost@protonmail.com>
2024-01-08 22:59:28 +0100

    Updated config

--- .config/swaylock/unlock.sh
+++ .config/swaylock/unlock.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MOUSE=$(ratbagctl | sed 's/ *\:.*//')
 
-ckb-next -p main -m blue &
-ratbagctl $MOUSE led 0 set color 03FDFC &
-flatpak run org.openrgb.OpenRGB -d "ASUS ROG STRIX X570-F GAMING" -z 0 -m static -c 03FDFC &
+ckb-next -p main -m orange\/brown &
+ratbagctl $MOUSE led 0 set color 591E01 &
+flatpak run org.openrgb.OpenRGB -d "ASUS ROG STRIX X570-F GAMING" -z 0 -m static -c 591E01 &
 swaymsg "output * dpms on" &
 
 wait
